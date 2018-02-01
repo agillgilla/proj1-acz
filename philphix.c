ddecce83ab3543ca2915f81938f09d28a03981c6
Arjun Gill <agillgilla@gmail.com>
2018-01-31 20:20:47 -0800

    Removed all frees

--- philphix.c
+++ philphix.c
@@ -229,7 +229,7 @@ int readNextPair(FILE *inputFile) { /* Read next key, value pair and enter into 
   fprintf(stderr, value);*/
   insertData(dictionary, key, value);
 
-  /*free(large_charbuff);*/
+  free(large_charbuff);
 
   return 1;
 }
@@ -345,9 +345,9 @@ int processNextWord() { /* Read next word and handle according to replace rules.
         }
       }
 
-      /*free(large_charbuff);
+      free(large_charbuff);
       free(word);
-      free(word_original);*/
+      free(word_original);
 
       
 
